d60f485217eb7e914b9a8015d636e6287ae1a823
maxwellc2003 <107020453+maxwellc2003@users.noreply.github.com>
2022-08-21 18:33:15 -0700

    initial page setup

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// fetch("https://api.openweathermap.org/data/3.0/onecall?lat=38.58&lon=121.49&appid=4b7771966b72932cdb6e3f82d9e9915b")
+//     .then(response => response.json())
+//     .then(data => console.log(data)); 
+console.log("hey")
