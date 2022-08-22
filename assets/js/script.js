ae43a8e231403151b4f40a1b398edacdee112f40
maxwellc2003 <107020453+maxwellc2003@users.noreply.github.com>
2022-08-22 10:34:32 -0700

    javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
-// fetch("https://api.openweathermap.org/data/3.0/onecall?lat=38.58&lon=121.49&appid=4b7771966b72932cdb6e3f82d9e9915b")
+// fetch("https://api.openweathermap.org/data/2.5/weather?lat=38.58&lon=-121.49&appid=4b7771966b72932cdb6e3f82d9e9915b")
 //     .then(response => response.json())
-//     .then(data => console.log(data)); 
-console.log("hey")
+//     .then(data => console.log(data));
+
+fetch("http://api.openweathermap.org/geo/1.0/direct?q=Sacramento&limit=1&appid=4b7771966b72932cdb6e3f82d9e9915b")
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
+
+
+
+    // .then((data) => {
+    //     var yourVariableName = data.result.public_holiday_description;
+    //     eventEl.innerHTML = yourVariableName;
+    // });
